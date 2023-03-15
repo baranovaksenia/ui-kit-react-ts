d12a6431d4df990351ac9c0dd4ef4c526224182b
Ksenia <baranova-ksenia1990@gmail.com>
2023-03-15 16:37:20 +0200

    add Button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Layout } from "components";
 
 import "./App.scss";
+import { Button } from "ui-kit";
 
 export const App: React.FC = () => {
+  const handleButtonClick = (event: React.MouseEvent) => {
+    console.log("[button click event]", event);
+  };
+
   return (
     <>
       <Layout>
         <h1>UI library</h1>
+        <h2>BUTTON</h2>
+        <Button isDisabled={true} onClick={handleButtonClick}>
+          submit
+        </Button>
       </Layout>
     </>
   );
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,19 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react'
 
-import { Button } from './Button';
+import { Button } from 'ui-kit';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+export default { title: "Button" }
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+export const stories = () => (
+	<div>
+		<div className='story'>
+			<label>Button</label>
+			<Button>Send</Button>
+		</div>
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+		<div className='story'>
+			<label>Button Disabled</label>
+			<Button isDisabled={true}>Disabled</Button>
+		</div>
+	</div>
+)
