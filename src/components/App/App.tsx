770fa09c0b8705850c80ccab6eec69f4942f2a88
Ksenia <baranova-ksenia1990@gmail.com>
2023-03-15 12:38:07 +0200

    project initilization

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "components";
+
+import "./App.scss";
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Layout>
+        <h1>UI library</h1>
+      </Layout>
+    </>
+  );
+};
